0c7820d77a5196ca3d510d3b5c7761baa386e484
Bruno08808 <brunoevangelista233@gmail.com>
2025-05-20 14:33:37 +0100

    imports

--- dev/app/src/main/java/pt/ipsantarem/apptroka/MainActivity.kt
+++ dev/app/src/main/java/pt/ipsantarem/apptroka/MainActivity.kt
@@ -5,6 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.app.DatePickerDialog
+import android.graphics.Typeface
+import android.view.Gravity
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Calendar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
